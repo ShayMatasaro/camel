31a9d0d93a3feaa8495330958d4988d0383e636b
Shay Matasaro <smatasar@redhat.com>
2013-09-15 15:13:43 -0400

    CAMEL-6753 CamelContextBean now returns an empty set

--- components/camel-cdi/src/main/java/org/apache/camel/cdi/internal/CamelContextBean.java
+++ components/camel-cdi/src/main/java/org/apache/camel/cdi/internal/CamelContextBean.java
@@ -40,94 +40,97 @@ import org.apache.deltaspike.core.api.literal.DefaultLiteral;
  */
 @SuppressWarnings("unchecked")
 public class CamelContextBean implements Bean<CdiCamelContext> {
+	private final BeanManager beanManager;
+	private final String name;
+	private final String camelContextName;
+	private final InjectionTarget<CdiCamelContext> target;
+	private final CamelContextConfig config;
 
-    private final BeanManager beanManager;
-    private final String name;
-    private final String camelContextName;
-    private final InjectionTarget<CdiCamelContext> target;
-    private final CamelContextConfig config;
+	public CamelContextBean(BeanManager beanManager) {
+		this(beanManager, "CamelContext", "", new CamelContextConfig());
+	}
 
-    public CamelContextBean(BeanManager beanManager) {
-        this(beanManager, "CamelContext", "", new CamelContextConfig());
-    }
+	public CamelContextBean(BeanManager beanManager, String name,
+			String camelContextName, CamelContextConfig config) {
+		this.beanManager = beanManager;
+		this.name = name;
+		this.camelContextName = camelContextName;
+		this.config = config;
+		this.target = beanManager.createInjectionTarget(beanManager
+				.createAnnotatedType(CdiCamelContext.class));
+	}
 
-    public CamelContextBean(BeanManager beanManager, String name, String camelContextName,
-                            CamelContextConfig config) {
-        this.beanManager = beanManager;
-        this.name = name;
-        this.camelContextName = camelContextName;
-        this.config = config;
-        this.target = beanManager.createInjectionTarget(beanManager.createAnnotatedType(CdiCamelContext.class));
-    }
+	@Override
+	public CdiCamelContext create(CreationalContext<CdiCamelContext> context) {
+		CdiCamelContext camelContext = target.produce(context);
+		if (ObjectHelper.isNotEmpty(camelContextName)) {
+			camelContext.setName(camelContextName);
+		}
+		target.postConstruct(camelContext);
+		context.push(camelContext);
+		return camelContext;
+	}
 
-    @Override
-    public CdiCamelContext create(CreationalContext<CdiCamelContext> context) {
-        CdiCamelContext camelContext = target.produce(context);
-        if (ObjectHelper.isNotEmpty(camelContextName)) {
-            camelContext.setName(camelContextName);
-        }
-        target.postConstruct(camelContext);
-        context.push(camelContext);
-        return camelContext;
-    }
+	@Override
+	public void destroy(CdiCamelContext instance,
+			CreationalContext<CdiCamelContext> context) {
+		target.preDestroy(instance);
+		target.dispose(instance);
+		context.release();
+	}
 
-    @Override
-    public void destroy(CdiCamelContext instance, CreationalContext<CdiCamelContext> context) {
-        target.preDestroy(instance);
-        target.dispose(instance);
-        context.release();
-    }
+	@Override
+	public Set<Type> getTypes() {
+		return new HashSet<Type>(Arrays.asList(Object.class,
+				CamelContext.class, CdiCamelContext.class));
+	}
 
-    @Override
-    public Set<Type> getTypes() {
-        return new HashSet<Type>(Arrays.asList(Object.class, CamelContext.class, CdiCamelContext.class));
-    }
+	@Override
+	public Set<Annotation> getQualifiers() {
+		return new HashSet<Annotation>(Arrays.asList(new DefaultLiteral(),
+				new AnyLiteral()));
+	}
 
-    @Override
-    public Set<Annotation> getQualifiers() {
-        return new HashSet<Annotation>(Arrays.asList(new DefaultLiteral(), new AnyLiteral()));
-    }
+	@Override
+	public Class<? extends Annotation> getScope() {
+		return ApplicationScoped.class;
+	}
 
-    @Override
-    public Class<? extends Annotation> getScope() {
-        return ApplicationScoped.class;
-    }
+	@Override
+	public String getName() {
+		return name;
+	}
 
-    @Override
-    public String getName() {
-        return name;
-    }
+	@Override
+	public boolean isNullable() {
+		return false;
+	}
 
-    @Override
-    public boolean isNullable() {
-        return false;
-    }
+	@Override
+	public Set<InjectionPoint> getInjectionPoints() {
+		return target.getInjectionPoints();
+	}
 
-    @Override
-    public Set<InjectionPoint> getInjectionPoints() {
-        return target.getInjectionPoints();
-    }
+	@Override
+	public Class<?> getBeanClass() {
+		return CdiCamelContext.class;
+	}
 
-    @Override
-    public Class<?> getBeanClass() {
-        return CdiCamelContext.class;
-    }
+	@Override
+	public Set<Class<? extends Annotation>> getStereotypes() {
+		return new HashSet<Class<? extends Annotation>>();
+	}
 
-    @Override
-    public Set<Class<? extends Annotation>> getStereotypes() {
-        return null;
-    }
+	@Override
+	public boolean isAlternative() {
+		return false;
+	}
 
-    @Override
-    public boolean isAlternative() {
-        return false;
-    }
+	public String getCamelContextName() {
+		return camelContextName;
+	}
 
-    public String getCamelContextName() {
-        return camelContextName;
-    }
-
-    public void configureCamelContext(CdiCamelContext camelContext) {
-        config.configure(camelContext, beanManager);
-    }
+	public void configureCamelContext(CdiCamelContext camelContext) {
+		config.configure(camelContext, beanManager);
+	}
 }
